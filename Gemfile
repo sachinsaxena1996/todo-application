9f96ff96e2fbe8e944c81bfdd3728cd77f2ed124
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2019-02-18 12:16:31 +0530

    installed and configured rack-cors

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 
 gem 'mongoid', '~> 5.0', '>= 5.0.2'
-
+gem 'rack-cors', require: 'rack/cors'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,21 @@ module TodoApplication
     # config.i18n.load_path += Dir[Rails.root.join('my',
     # 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+
+        resource '/cors',
+          :headers => :any,
+          :methods => [:post],
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
   end
 end
